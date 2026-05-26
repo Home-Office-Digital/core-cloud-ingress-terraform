1e3105c1de984711171ed230c37ee6672c9eb0fb
Venkata Para <venkata.para@digital.homeoffice.gov.uk>
2026-05-26 10:33:08 +0100

    Add AnonymousIpList managed rule to custom ALB WAF ACL

--- modules/ingress/custom-alb/main.tf
+++ modules/ingress/custom-alb/main.tf
@@ -170,8 +170,30 @@ resource "aws_wafv2_web_acl" "tenant_alb" {
   }
 
   rule {
-    name     = "AWSManagedRulesKnownBadInputsRuleSet"
+    name     = "AWSManagedRulesAnonymousIpList"
     priority = 20
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAnonymousIpList"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesAnonymousIpList"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 30
 
     override_action {
       none {}
